ffb322d8b1b754a375235d527dc4be19aa92883c
MaxainNN <imenolys23@gmail.com>
2023-10-01 16:58:42 +0300

    Second commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,7 @@
-fun main(args: Array<String>) {
+fun main() {
     val secretary = Secretary(name="Max")
-
+// This is a comment
+    secretary.translate()
+    println("Bonus:${secretary.bonus()}")
 
 }
